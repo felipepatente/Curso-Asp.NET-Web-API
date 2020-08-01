f516c45ec4204e193fa506ee9f80a0de1d5e81d9
felipepatente <felipepatente@yahoo.com.br>
2020-08-01 11:39:01 -0300

    Inserindo Owin
    Classe de inicialização do Owin (Startup)

---
+++ WebApp/WebApp/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+    }
+}
